e5be8025e54c2d01815da256cad008ff69f3eed8
천광규 <chonkk@castis.com>
2021-07-27 23:10:33 +0900

    Add first

---
+++ src/main/java/com/jamsun2/application/VuejsSpringbootApplication.java
@@ -0,0 +1,13 @@
+package com.jamsun2.application;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VuejsSpringbootApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(VuejsSpringbootApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/jamsun2/application/VuejsSpringbootApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jamsun2.application;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VuejsSpringbootApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
